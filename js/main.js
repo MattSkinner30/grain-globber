b9e50ec8a9cc203d300447f5fc306e9b6f38985b
MattSkinner30 <MattSkinner@Matts-MacBook-Pro.local>
2016-03-10 13:29:11 -0500

    Created Variables

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var peanuts = {
+	name: 'Peanuts',
+	img: 'images/peanuts.jpg',
+	desc: 'First cultivated in the valleys of Paraguay.'
+};
+
+var beans = {
+	name: 'Beans',
+	img: 'images/beans.jpg',
+	desc: 'A summer crop that needs warm temperatures.'
+};
+
+var lentils = {
+	name: 'Lentils',
+	img: 'images/lentils.jpg',
+	desc: 'An edible pulse from a bushy annual plant.'
+};
+
+
